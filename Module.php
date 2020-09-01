5b13dc51c310bfd1cb90a50331410e404570b701
paskuale75 <pellicaipasquale@gmail.com>
2020-09-01 20:43:32 +0200

    first commit

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace paskuale75\anagrafica;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\Module as YiiModule;
+use yii\base\BootstrapInterface;
+use yii\console\Application as ConsoleApplication;
+use yii\web\Application as WebApplication;
+use yii\web\GroupUrlRule;
+use yii\web\UserEvent;
+use yii\helpers\ArrayHelper;
+
+class Module extends YiiModule implements BootstrapInterface
+{
+
+
+}
