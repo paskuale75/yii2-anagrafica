5f087efca95313a9647ef6ff0afeeb0b83bc97bc
paskuale75 <pellicaipasquale@gmail.com>
2020-09-07 00:30:07 +0200

    old actions removed

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -55,90 +55,12 @@ class DefaultController extends Controller
         }
         echo Json::encode($out);
     }
-    public function actionAlunniListConCfClasse($q = null) {
-        $query = new Query;
-        $tableName = Alunno::tableName();
-        $anagraficaTable = Anagrafica::tableName().' anag'; //anag is alias
+    
 
-        $query->select('anag.id, anagrafica_id,mns_alunni.id, anag.ragione_sociale_1, anag.ragione_sociale_2,codfis')
-            ->from($tableName)
-            ->leftJoin($anagraficaTable, 'anag.id = anagrafica_id')
-            ->where(
-                'CONCAT_WS(" ",anag.ragione_sociale_1,anag.ragione_sociale_2)'
-                .' LIKE "%' . $q . '%"'
-            )
-            ->orderBy('anag.ragione_sociale_1');
-        $command = $query->createCommand();
-        $data = $command->queryAll();
-        $out = [];
-        foreach ($data as $d) {
-            $model = Alunno::findOne($d['id']);
-            $option = $model->option;
-            
-            $classe = isset($option)?$option->classe:'';
-            $nomeClasse = isset($classe->fullName)?$classe->fullName:'';
-            $scuola = isset($option)?$option->scuola:'';
-            $nomeScuola = isset($scuola->nome)?$scuola->nome:'';
-            $out[] = [
-                'id'=>$d['id'],
-                'value' => $d['ragione_sociale_1'].' '.$d['ragione_sociale_2'].' - '.$d['codfis'].' - '.$nomeClasse.' - '.$nomeScuola
-            ];
-        }
-        echo Json::encode($out);
-    }
-
-    public function actionGenitoriList($q = null) {
-        $query = new Query;
-        $tableName = Genitore::tableName();
-        $anagraficaTable = Anagrafica::tableName().' anag'; //anag is alias
-
-        $query->select('anag.id, anagrafica_id, genitore_id, anag.ragione_sociale_1, anag.ragione_sociale_2')
-            ->from($tableName)
-            ->leftJoin($anagraficaTable, 'id = anagrafica_id')
-            ->where(
-                'CONCAT_WS(" ",anag.ragione_sociale_1,anag.ragione_sociale_2)'
-                .' LIKE "%' . $q . '%"'
-            )
-            ->orderBy('anag.ragione_sociale_1');
-        $command = $query->createCommand();
-        $data = $command->queryAll();
-        $out = [];
-        foreach ($data as $d) {
-            $out[] = [
-                'id'=>$d['genitore_id'],
-                'value' => $d['ragione_sociale_1'].' '.$d['ragione_sociale_2']
-            ];
-        }
-        echo Json::encode($out);
-    }
+    
 
 
-    public function actionAlunniList($q = null) {
-        $query = new Query;        
-        $tableName = Alunno::tableName().' alu';
-        $alunnoKey = 'alu.ID';
-        $anagraficaTable = Anagrafica::tableName().' anag'; //anag is alias
-
-        $query->select('anag.id, anagrafica_id, '.$alunnoKey.' AS alunnoID, anag.ragione_sociale_1, anag.ragione_sociale_2')
-            ->from($tableName)
-            ->leftJoin($anagraficaTable, 'anag.id = anagrafica_id')
-            ->where(
-                'CONCAT_WS(" ",anag.ragione_sociale_1,anag.ragione_sociale_2)'
-                .' LIKE "%' . $q . '%"'
-            )
-            ->orderBy('anag.ragione_sociale_1');
-        $command = $query->createCommand();
-        $data = $command->queryAll();
-        $out = [];
-        foreach ($data as $d) {
-            $out[] = [
-                'id'=>$d['alunnoID'],
-                'value' => $d['ragione_sociale_1'].' '.$d['ragione_sociale_2']
-            ];
-        }
-        echo Json::encode($out);
-    }
-
+    
     public function actionCalculateCf($flag_nazione=false){
 
         if($flag_nazione){
